d9df254b0d673026e38ecf2b4339bf6568097638
acruz2332 <akbar.fajar2301@mail.ugm.ac.id>
2023-02-22 12:04:24 +0700

    Initial commit

---
+++ app/src/main/java/com/ppab/ppab2p1_tugas/FragmentTugas.java
@@ -0,0 +1,24 @@
+package com.ppab.ppab2p1_tugas;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentTugas extends Fragment {
+
+    public FragmentTugas() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tugas, container, false);
+
+    }
+}
